586c244cc301580bd2a85e357d31681ea5d9c6c2
kwakp <kwakp05@gmail.com>
2026-02-22 13:11:14 -0500

    Refactor to types

--- Tilt/ParserUtils.h
+++ Tilt/ParserUtils.h
@@ -59,3 +59,71 @@ struct get_variant_return
     using type = std::invoke_result_t<P, std::string_view>;
 };
 
+constexpr bool is_whitespace(char c)
+{
+    return c == ' ' || c == '\n';
+}
+
+constexpr bool is_crlf(char c1, char c2)
+{
+    return c1 == '\r' && c2 == '\n';
+}
+
+constexpr bool is_alpha(char c)
+{
+    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+}
+
+constexpr bool is_digit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+constexpr bool is_identifier(char c)
+{
+    return is_alpha(c) || is_digit(c) || c == '_';
+}
+
+constexpr bool is_control(char c)
+{
+    return c == '-' || c == '>';
+}
+
+std::string_view consume_whitespace(std::string_view input)
+{
+    std::size_t i = 0;
+    while (true)
+    {
+        if (i < input.size() && is_whitespace(input[i]))
+            i++;
+        else if (i + 1 < input.size() && is_crlf(input[i], input[i + 1]))
+            i += 2;
+        else
+            break;
+    }
+    return input.substr(i);
+}
+
+namespace
+{
+template <class Func>
+struct next_impl
+{
+    auto operator()(Parsed auto val) const
+    {
+		using T = std::decay_t<decltype(val)>;
+		std::string_view remainder;
+		if constexpr (Parsed<T>)
+			remainder = consume_whitespace(val.remainder);
+		else
+			remainder = std::visit([](auto& x)
+			{
+					return consume_whitespace(x.remainder);
+			}, val);
+		return Func{}(remainder);
+    }
+};
+}
+
+template <class Func>
+constexpr auto next = next_impl<Func>{};
--- Tilt/Tilt.cpp
+++ Tilt/Tilt.cpp
@@ -149,201 +149,169 @@ struct ParsedExpression
     std::string_view remainder;
 };
 
-constexpr bool is_whitespace(char c)
+struct parse_identifier
 {
-    return c == ' ' || c == '\n';
-}
+	std::expected<ParsedIdentifier, ParseError> operator()(std::string_view input) const
+	{
+		if (input.empty())
+			return std::unexpected("empty");
+		if (!is_identifier(input.front()))
+			return std::unexpected("bad char");
 
-constexpr bool is_crlf(char c1, char c2)
+		auto it = std::find_if_not(input.begin(), input.end(), is_identifier);
+		size_t identifier_end = std::distance(input.begin(), it);
+		return ParsedIdentifier{
+			.identifier = input.substr(0, identifier_end),
+			.remainder = input.substr(identifier_end)
+		};
+	}
+};
+
+struct parse_operator
 {
-    return c1 == '\r' && c2 == '\n';
-}
+	std::expected<ParsedOperator, ParseError> operator()(std::string_view input) const
+	{
+		auto it = std::find_if_not(input.begin(), input.end(), is_control);
+		size_t operator_end = std::distance(input.begin(), it);
+		std::string_view op = input.substr(0, operator_end);
+		if (op == "->")
+			return ParsedFunctionOperator{
+				.remainder = input.substr(operator_end)
+			};
+		return std::unexpected("Invalid operator: " + std::string{ op });
+	}
+};
 
-constexpr bool is_alpha(char c)
+struct parse_command_name
 {
-    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
-}
+	std::expected<std::variant<ParsedCheckCommand, ParsedEvalCommand>, ParseError> operator()(std::string_view input) const
+	{
+		return parse_identifier{}(input).and_then([](ParsedIdentifier x) -> std::expected<std::variant<ParsedCheckCommand, ParsedEvalCommand>, ParseError>
+		{
+				if (x.identifier == "check")
+					return ParsedCheckCommand{ .remainder = x.remainder };
+				else if (x.identifier == "eval")
+					return ParsedEvalCommand{ .remainder = x.remainder };
+				else
+					return std::unexpected("Invalid command: " + std::string(x.identifier));
+		});
+	}
+};
 
-constexpr bool is_digit(char c)
+struct parse_expression
 {
-    return c >= '0' && c <= '9';
-}
+	std::expected<ParsedExpression, ParseError> operator()(std::string_view input) const
+	{
+		struct test1 {};
+		struct test2 {};
+		std::variant<test1, test2> state{ test1{} };
+		while (true)
+		{
+			auto result = std::visit([input](auto x)
+			{
+					using T = std::decay_t<decltype(x)>;
+					if constexpr (std::is_same_v<T, test1>)
+					{
+						//return parse_identifier(input);
+						return 3;
+					}
+					else if constexpr (std::is_same_v<T, test2>)
+					{
+						//return parse_identifier(input)
+						//    .or_else([input]() {return parse_identifier(input); });
+						return 2;
+					}
+					else
+						static_assert(false, "should be unreachable");
+			}, state);
+			break;
 
-constexpr bool is_identifier(char c)
+		}
+		//parse_identifier(input).and_then(next(parse_operator));
+
+		return std::unexpected("how do i do this");
+	}
+};
+
+struct parse_hash
 {
-    return is_alpha(c) || is_digit(c) || c == '_';
-}
+	std::expected<ParsedHash, ParseError> operator()(std::string_view input) const
+	{
+		if (input.empty())
+			return std::unexpected("empty");
+		if (input.front() == '#')
+			return ParsedHash{ .remainder=input.substr(1) };
+		return std::unexpected("Expected hash");
+	}
+};
 
-constexpr bool is_control(char c)
+struct parse_colon
 {
-    return c == '-' || c == '>';
-}
+	std::expected<ParsedColon, ParseError> operator()(std::string_view input) const
+	{
+		if (input.empty())
+			return std::unexpected("empty");
+		if (input.front() == ':')
+			return ParsedColon{ .remainder=input.substr(1) };
+		return std::unexpected("Expected colon");
+	}
+};
 
-std::string_view consume_whitespace(std::string_view input)
+struct parse_assignment
 {
-    std::size_t i = 0;
-    while (true)
-    {
-        if (i < input.size() && is_whitespace(input[i]))
-            i++;
-        else if (i + 1 < input.size() && is_crlf(input[i], input[i + 1]))
-            i += 2;
-        else
-            break;
-    }
-    return input.substr(i);
-}
+	std::expected<ParsedAssignment, ParseError> operator()(std::string_view input) const
+	{
+		if (input.size() < 2)
+			return std::unexpected("not enough chars for assingmnent operator");
+		if (input.starts_with(":="))
+			return ParsedAssignment{ input.substr(2) };
+		return std::unexpected("Expected assignment operator");
+	}
+};
 
-template <class Func>
-auto next(Func const& func)
+struct parse_type_name
 {
-    return [&func](auto val) {
-        using T = std::decay_t<decltype(val)>;
-        std::string_view remainder;
-        if constexpr (Parsed<T>)
-            remainder = consume_whitespace(val.remainder);
-        else
-            remainder = std::visit([](auto& x)
-            {
-                    return consume_whitespace(x.remainder);
-            }, val);
-        return func(remainder);
-    };
-}
+	std::expected<ParsedType, ParseError> operator()(std::string_view input) const
+	{
+		return parse_identifier{}(input).and_then(
+			[](ParsedIdentifier x) -> std::expected<ParsedType, ParseError> {
+				return ParsedType{ .name = x.identifier, .remainder = x.remainder };
+			});
+	}
+};
 
-std::expected<ParsedIdentifier, ParseError> parse_identifier(std::string_view input)
+struct parse_nat_literal
 {
-    if (input.empty())
-        return std::unexpected("empty");
-    if (!is_identifier(input.front()))
-        return std::unexpected("bad char");
-
-    auto it = std::find_if_not(input.begin(), input.end(), is_identifier);
-    size_t identifier_end = std::distance(input.begin(), it);
-    return ParsedIdentifier{
-        .identifier = input.substr(0, identifier_end),
-        .remainder = input.substr(identifier_end)
-    };
-}
-
-std::expected<ParsedOperator, ParseError> parse_operator(std::string_view input)
-{
-    auto it = std::find_if_not(input.begin(), input.end(), is_control);
-    size_t operator_end = std::distance(input.begin(), it);
-    std::string_view op = input.substr(0, operator_end);
-    if (op == "->")
-        return ParsedFunctionOperator{
-            .remainder = input.substr(operator_end)
-        };
-    return std::unexpected("Invalid operator: " + std::string{ op });
-}
-
-std::expected<std::variant<ParsedCheckCommand, ParsedEvalCommand>, ParseError> parse_command_name(std::string_view input)
-{
-    return parse_identifier(input).and_then([](ParsedIdentifier x) -> std::expected<std::variant<ParsedCheckCommand, ParsedEvalCommand>, ParseError>
-    {
-            if (x.identifier == "check")
-                return ParsedCheckCommand{ .remainder = x.remainder };
-            else if (x.identifier == "eval")
-                return ParsedEvalCommand{ .remainder = x.remainder };
-            else
-                return std::unexpected("Invalid command: " + std::string(x.identifier));
-    });
-}
-
-std::expected<ParsedExpression, ParseError> parse_expression(std::string_view input)
-{
-    struct test1 {};
-    struct test2 {};
-    std::variant<test1, test2> state{ test1{} };
-    while (true)
-    {
-        auto result = std::visit([input](auto x)
-        {
-                using T = std::decay_t<decltype(x)>;
-                if constexpr (std::is_same_v<T, test1>)
-                {
-                    //return parse_identifier(input);
-                    return 3;
-                }
-                else if constexpr (std::is_same_v<T, test2>)
-                {
-                    //return parse_identifier(input)
-                    //    .or_else([input]() {return parse_identifier(input); });
-                    return 2;
-                }
-                else
-                    static_assert(false, "should be unreachable");
-        }, state);
-        break;
-
-    }
-    parse_identifier(input).and_then(next(parse_operator));
-
-    return std::unexpected("how do i do this");
-}
-
-std::expected<ParsedHash, ParseError> parse_hash(std::string_view input)
-{
-    if (input.empty())
-        return std::unexpected("empty");
-    if (input.front() == '#')
-        return ParsedHash{ .remainder=input.substr(1) };
-    return std::unexpected("Expected hash");
-}
-
-std::expected<ParsedColon, ParseError> parse_colon(std::string_view input)
-{
-    if (input.empty())
-        return std::unexpected("empty");
-    if (input.front() == ':')
-        return ParsedColon{ .remainder=input.substr(1) };
-    return std::unexpected("Expected colon");
-}
-
-std::expected<ParsedAssignment, ParseError> parse_assignment(std::string_view input)
-{
-    if (input.size() < 2)
-        return std::unexpected("not enough chars for assingmnent operator");
-    if (input.starts_with(":="))
-        return ParsedAssignment{ input.substr(2) };
-    return std::unexpected("Expected assignment operator");
-}
-
-std::expected<ParsedType, ParseError> parse_type_name(std::string_view input)
-{
-    return parse_identifier(input).and_then(
-        [](ParsedIdentifier x) -> std::expected<ParsedType, ParseError> {
-            return ParsedType{ .name = x.identifier, .remainder = x.remainder };
-        });
-}
-
-std::expected<ParsedNatLiteral, ParseError> parse_nat_literal(std::string_view input)
-{
-    auto it = std::find_if_not(input.begin(), input.end(), is_digit);
-    size_t literal_end = std::distance(input.begin(), it);
-    if (literal_end == 0)
-        return std::unexpected("Expected Nat literal");
-    return ParsedNatLiteral{
-        .literal = input.substr(0, literal_end),
-        .remainder = input.substr(literal_end)
-    };
-}
-
-std::expected<ParsedBoolLiteral, ParseError> parse_bool_literal(std::string_view input)
-{
-    auto it = std::find_if_not(input.begin(), input.end(), is_alpha);
-    size_t literal_end = std::distance(input.begin(), it);
-    if (literal_end == 0)
-        return std::unexpected("Expected Bool literal");
-    auto literal = input.substr(0, literal_end);
-    if (literal == "true" || literal == "false")
-		return ParsedBoolLiteral{
+	std::expected<ParsedNatLiteral, ParseError> operator()(std::string_view input) const
+	{
+		auto it = std::find_if_not(input.begin(), input.end(), is_digit);
+		size_t literal_end = std::distance(input.begin(), it);
+		if (literal_end == 0)
+			return std::unexpected("Expected Nat literal");
+		return ParsedNatLiteral{
 			.literal = input.substr(0, literal_end),
 			.remainder = input.substr(literal_end)
 		};
-    return std::unexpected("invalid bool literal: " + std::string(literal));
-}
+	}
+};
+
+struct parse_bool_literal
+{
+	std::expected<ParsedBoolLiteral, ParseError> operator()(std::string_view input) const
+	{
+		auto it = std::find_if_not(input.begin(), input.end(), is_alpha);
+		size_t literal_end = std::distance(input.begin(), it);
+		if (literal_end == 0)
+			return std::unexpected("Expected Bool literal");
+		auto literal = input.substr(0, literal_end);
+		if (literal == "true" || literal == "false")
+			return ParsedBoolLiteral{
+				.literal = input.substr(0, literal_end),
+				.remainder = input.substr(literal_end)
+			};
+		return std::unexpected("invalid bool literal: " + std::string(literal));
+	}
+};
 
 template<Keyword k>
 std::expected<ParsedKeyword, ParseError> expect_keyword(ParsedKeyword val)
@@ -353,13 +321,22 @@ std::expected<ParsedKeyword, ParseError> expect_keyword(ParsedKeyword val)
     return std::unexpected("wrong keyword");
 }
 
-template <class Func>
-auto immediate(Func const& func)
+namespace
 {
-    return [&func](auto val) {
-        return func(val.remainder);
-    };
+template <class Func>
+struct immediate_impl
+{
+    auto operator()(Parsed auto val) const
+    {
+		using T = std::decay_t<decltype(val)>;
+		std::string_view remainder;
+		return Func{}(remainder);
+    }
+};
 }
+
+template <class Func>
+constexpr auto immediate = immediate_impl<Func>{};
 
 std::expected<ParsedDummy, ParseError> begin_parse(std::string_view input)
 {
@@ -375,14 +352,17 @@ auto effect(Func const& func)
 	};
 }
 
-std::expected<ParsedKeyword, ParseError> parse_keyword(std::string_view input)
+struct parse_keyword
 {
-    if (input.starts_with("def"))
+    std::expected<ParsedKeyword, ParseError> operator()(std::string_view input) const
     {
-        return ParsedKeyword{ .keyword = Keyword::DEF, .remainder = input.substr(3) };
-    }
-    return std::unexpected("Invalid keyword");
-}
+		if (input.starts_with("def"))
+		{
+			return ParsedKeyword{ .keyword = Keyword::DEF, .remainder = input.substr(3) };
+		}
+		return std::unexpected("Invalid keyword");
+	}
+};
 
 std::expected<Constant, ParseError> parse_constant(std::string_view input)
 {
@@ -390,23 +370,23 @@ std::expected<Constant, ParseError> parse_constant(std::string_view input)
     std::string name;
     std::string type;
     auto result = begin_parse(input)
-        .and_then(next(parse_keyword))
+        .and_then(next<parse_keyword>)
         .and_then(expect_keyword<Keyword::DEF>)
-        .and_then(next(parse_identifier))
+        .and_then(next<parse_identifier>)
         .and_then(effect([&name](ParsedIdentifier x) {name = x.identifier; }))
-        .and_then(next(parse_colon))
-        .and_then(next(parse_type_name))
+        .and_then(next<parse_colon>)
+        .and_then(next<parse_type_name>)
         .and_then(effect([&type](ParsedType x) {type = x.name; }))
-        .and_then(next(parse_assignment))
+        .and_then(next<parse_assignment>)
         .and_then([&type](auto x) -> std::expected<std::variant<ParsedNatLiteral, ParsedBoolLiteral>, ParseError>
         {
                 if (type == "Nat")
                 {
-                    return next(parse_nat_literal)(x);
+                    return next<parse_nat_literal>(x);
                 }
                 else if (type == "Bool")
                 {
-                    return next(parse_bool_literal)(x);
+                    return next<parse_bool_literal>(x);
 
                 }
                 else
@@ -444,9 +424,9 @@ std::expected<Command, ParseError> parse_command(std::string_view input)
     std::string name;
     std::string type;
     auto result = begin_parse(input)
-        .and_then(next(parse_hash))
-        .and_then(immediate(parse_command_name))
-        .and_then(next(parse_expression));
+        .and_then(next<parse_hash>)
+        .and_then(immediate<parse_command_name>);
+        //.and_then(next<parse_expression>);
     return std::unexpected("HI");
 }
 
